2155b0e006c605d59eb6480cd031f7884d7cf796
Elena1965 <turenkova_elena@mail.ru>
2017-07-12 18:37:42 +0300

    add selenium-test

---
+++ selenium-test/src/test/java/SecondTestGoogle.java
@@ -0,0 +1,44 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class SecondTestGoogle {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void secondTestGoogle() {
+        wd.get("http://www.seleniumhq.org/docs/03_webdriver.jsp");
+        wd.findElement(By.cssSelector("h2")).click();
+        wd.findElement(By.id("by-tag-name")).click();
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
